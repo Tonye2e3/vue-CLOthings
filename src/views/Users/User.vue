618ac0b502ba5f5278f33245d93897bae06a0729
Tony <asd.46563@gmail.com>
2026-08-06 11:36:40 +0800

    修改User資料夾名稱

--- src/views/Users/User.vue
+++ src/views/Users/User.vue
@@ -1,7 +1,7 @@
 <script setup>
-import AccountCard from './Card/AccountCard.vue'
-import AddressCard from './Card/AddressCard.vue'
-import ProfileCard from './Card/ProfileCard.vue'
+import AccountCard from './UserCard/AccountCard.vue'
+import AddressCard from './UserCard/AddressCard.vue'
+import ProfileCard from './UserCard/ProfileCard.vue'
 </script>
 
 <template>
