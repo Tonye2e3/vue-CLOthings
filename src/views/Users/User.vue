8a916a080bba83c9a7804ea69f34b7fa023f3e9b
Tony <asd.46563@gmail.com>
2026-08-07 09:53:19 +0800

    修改命名方式

--- src/views/Users/User.vue
+++ src/views/Users/User.vue
@@ -2,7 +2,7 @@
 import AccountCard from './UserCard/AccountCard.vue'
 import AddressCard from './UserCard/AddressCard.vue'
 import ProfileCard from './UserCard/ProfileCard.vue'
-import oAuthCard from './UserCard/oAuthCard.vue'
+import oAuthCard from './UserCard/OAuthCard.vue'
 </script>
 
 <template>
