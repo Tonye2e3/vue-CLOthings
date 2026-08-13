071d36aae64e0ce16bd763bb72d57bfcdaf88cad
Tony <asd.46563@gmail.com>
2026-08-13 10:56:16 +0800

    修改groupshop 使用者登入顯示按鈕

--- src/App.vue
+++ src/App.vue
@@ -71,13 +71,7 @@ function logout() {
         >
           <IconUser />
         </RouterLink>
-        <RouterLink
-          v-else
-          :to="{ name: 'login' }"
-          v-if="!isGroupSection"
-          class="icon-btn"
-          aria-label="帳號"
-        >
+        <RouterLink v-else :to="{ name: 'login' }" class="icon-btn" aria-label="帳號">
           <IconUser />
         </RouterLink>
         <RouterLink
