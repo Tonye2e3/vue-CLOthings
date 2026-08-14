886ab6790aac4e609999b6a17cace9fd04ac6f13
su <s991207@gmail.com>
2026-08-14 15:56:46 +0800

    增加第三方支付及修改內容

--- src/api/groupShopAdmin.js
+++ src/api/groupShopAdmin.js
@@ -48,6 +48,8 @@ export const deleteShipper = (id) => http.delete(`/GroupShipper/${id}`)
 
 // ---- 客服紀錄（管理端）----
 export const getAllCustomerService = () => http.get('/GroupCustomerService/admin/all').then(res => res.data)
+export const replyCustomerService = (id, replyContent) =>
+  http.put(`/GroupCustomerService/${id}/reply`, { replyContent }).then(res => res.data)
 
 // ---- 訂單管理 ----
 // status 選填：帶了就只回傳該狀態的訂單
