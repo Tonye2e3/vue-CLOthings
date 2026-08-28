5ca1e19ad0b886d4f7bcf175fb1529a5a9676231
Cui <Sandy881133@gmail.com>
2026-08-28 15:20:18 +0800

    addToCart、toggleFavorite、buyNow 開頭各加登入檢查 260828 15:19

--- src/views/Shop/ShopView.vue
+++ src/views/Shop/ShopView.vue
@@ -7,7 +7,7 @@ import CategoryShowcase from '@/components/home/CategoryShowcase.vue'
 <template>
   <div class="uniqlo-home">
     <main>
-      <!-- <h1>2026/08/26 15:02</h1> -->
+      <!-- <h1>2026/08/28 15:19</h1> -->
       <HeroCarousel />
       <ProductGrid />
     </main>
